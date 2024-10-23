f09c8a18f7627b992273824330255337a858bfff
Wilfredo <jrlopez85@gmail.com>
2024-10-22 23:06:27 -0500

    Top sellers section dynamically tagged and showing the information as needed with .sort to show highest seller. Loading skeleton implemented as well.

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTopSellers = async () => {
+    const response = await fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
+    const data = await response.json();
+    setTopSellers(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchTopSellers();
+  }, [])
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +31,41 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {loading 
+              ? new Array(12).fill(0).map((_, index) => (
                 <li key={index}>
+                  <div className="author_list_pp">
+                    <Link to="/author">
+                      <Skeleton width="50px" height="50px" borderRadius="50px" />
+                    </Link>
+                  </div>
+                  <div className="author_list_info">
+                    <div>
+                      <Skeleton width="90px" height="20px" borderRadius="5px" />
+                    </div>
+                    <div>
+                      <Skeleton width="50px" height="20px" borderRadius="5px" />
+                    </div>
+                  </div>
+                </li>
+              )) 
+              : topSellers.sort((a, b) => b.price - a.price).map((seller) => (
+                <li key={seller.index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={seller.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">
+                      {seller.authorName}
+                    </Link>
+                    <span>{seller.price} ETH</span>
                   </div>
                 </li>
               ))}
