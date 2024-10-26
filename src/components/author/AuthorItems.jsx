d6a661bcf2a3692c9944b9c80fcb0f7741fb4211
Wilfredo <jrlopez85@gmail.com>
2024-10-25 19:05:14 -0500

    Author section get redirected from explore and all section from homepage. Follow button increase and decreases follows depending on the state. Copy button copy wallet address. Loading skeleton on both author and items.

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,19 +1,66 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useLocation } from 'react-router-dom';
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const AuthorItems = () => {
+
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [authorItems, setAuthorItems] = useState(null);
+
+  useEffect(() => {
+    const fetchAuthorItems = async () => {
+      try {
+        const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`);
+        // console.log(data)
+        setAuthorItems(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchAuthorItems();
+  }, [id]);
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {loading 
+          ? new Array(8).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-12 col-xs-12" key={index}>
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Skeleton width="60px" height="60px" borderRadius="50px" />
+                </div>
+                <div className="de_countdown" style={{ right: 6, border: "none" }}>
+                  <Skeleton width="80px" height="20px" borderRadius="5px" />
+                </div>
+
+                <div className="nft__item_wrap">
+                  <Skeleton width="200px" height="250px" borderRadius="5px" top="28px" />
+                </div>
+
+                <div className="nft__item_info">
+                  <Skeleton width="80px" height="20px" borderRadius="5px" />
+                  <div>
+                    <Skeleton width="45px" height="20px" borderRadius="5px" />
+                    <Skeleton width="33px" height="20px" borderRadius="5px" left="62%" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )) 
+          : authorItems && authorItems.nftCollection.map((author) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={author.id}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={authorItems.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -37,7 +84,7 @@ const AuthorItems = () => {
                   </div>
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={author.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -45,12 +92,12 @@ const AuthorItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{author.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{author.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{author.likes}</span>
                   </div>
                 </div>
               </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { useParams, Link, useLocation, useNavigate } from 'react-router-dom';
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const {id} = useParams();
+  const [loading, setLoading] = useState(true);
+  const [authorItems, setAuthorItems] = useState([]);
+  const navigate = useNavigate();
+  const [follow, setFollow] = useState(false);
+  const [followCount, setFollowCount] = useState(0);
+  const [hasCopied, setHasCopied] = useState(false);
+
+  useEffect(() => {
+    const fetchAuthorItems = async () => {
+      try {
+        const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`);
+        setAuthorItems(data);
+        setFollowCount(data.followers); // Set initial follower count
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchAuthorItems();
+  }, [id]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(`${authorItems.address}`)
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+
+  const handleFollowCount = () => {
+    setFollow((prevFollow) => !prevFollow);
+    setFollowCount((prevCount) => (follow ? prevCount - 1 : prevCount + 1));
+  }
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -21,38 +60,71 @@ const Author = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+              { loading ? 
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <Skeleton width="120px" height="120px" borderRadius="80px" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                          <Skeleton width="60px" height="20px" borderRadius="5px" />
+                            <span className="profile_username">
+                              <Skeleton width="60px" height="20px" borderRadius="5px" />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton width="120px" height="20px" borderRadius="5px" />
+                            </span>
+                            
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton width="60px" height="20px" borderRadius="5px" />
+                        </div>
+                        <Skeleton width="60px" height="30px" borderRadius="5px" />
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
+                </div> 
+                :
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_avatar">
+                        <img src={authorItems.authorImage} alt="" />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {authorItems.authorName}
+                            <span className="profile_username">@{authorItems.tag}</span>
+                            <span id="wallet" className="profile_wallet">
+                              {authorItems.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text" onClick={handleCopy}>
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">{followCount} followers</div>
+                        <Link to="#" className="btn-main" onClick={handleFollowCount}>
+                          {follow ? "UnFollow" : "Follow"}
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-
+              }
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
                   <AuthorItems />
